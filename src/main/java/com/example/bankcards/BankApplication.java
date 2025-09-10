f38137495a94ab97b41a86ea51a8e9a896a81d2e
oybek <saydaliyev.dev@gmail.com>
2025-09-10 17:51:15 +0500

    clear utils

--- src/main/java/com/example/bankcards/BankApplication.java
+++ src/main/java/com/example/bankcards/BankApplication.java
@@ -1,26 +1,17 @@
 package com.example.bankcards;
 
-import com.example.bankcards.entity.AuthUser;
-import com.example.bankcards.entity.enums.AuthRole;
-import com.example.bankcards.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
     }
 
-//    @Bean
-    CommandLineRunner runner(PasswordEncoder passwordEncoder, UserRepository userRepository){
-        return args -> {
-            AuthUser auth = new AuthUser("Admin", passwordEncoder.encode("Admin"), AuthRole.ADMIN);
-            auth.setEmail("Admin");
-            auth.setFullName("Adminov Admin");
-            userRepository.save(auth);
-        };
-    }
+
 }
